77ca7807d54c0f24b6092939c6b732af7878055d
kotarogasu <kouta2607@gmail.com>
2019-11-26 21:31:09 +0900

    basic認証本番環境設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191125151404) do
+ActiveRecord::Schema.define(version: 20191125120932) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                                          null: false
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
-    t.string   "avatar"
-    t.text     "user_text",              limit: 65535
-    t.string   "first_name",                                        null: false
-    t.string   "last_name",                                         null: false
-    t.string   "first_name_kana",                                   null: false
-    t.string   "last_name_kana",                                    null: false
-    t.integer  "post_number"
-    t.integer  "phone_number"
-    t.integer  "prefecture",                           default: 0,  null: false
-    t.string   "city"
-    t.string   "town"
-    t.string   "building"
-    t.date     "birthday"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
