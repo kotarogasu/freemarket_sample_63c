09586907a8276c75a0f1f55eacabaefcf6436e20
yoshikawa keishi <a130809k@gmail.com>
2019-11-29 20:46:22 +0900

    トップページに商品のカードを表示させる

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,10 +3,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick 
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [500, 500]
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  if use_s3?
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
