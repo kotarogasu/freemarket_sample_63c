d80cdb3733e20dd218798ca29e9d03f40b056b8d
yoshikawa keishi <a130809k@gmail.com>
2019-11-29 15:19:32 +0900

    修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
   
   def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user = User.find(session[:user_id])
   end
   
 
