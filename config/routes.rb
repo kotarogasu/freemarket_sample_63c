f20c212ca866ebfa30b23758f253ea08ef9dfbc1
kotarogasu <kouta2607@gmail.com>
2019-12-01 16:39:28 +0900

    ルート調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'items#index'
 
   resources :signup do
     collection do
@@ -13,10 +15,6 @@ Rails.application.routes.draw do
   end
 
   delete '/logout', to: 'signup#destroy'
-
-
-  devise_for :users
-  root 'items#index'
 
   resources :items, only: [:new, :create]
 
