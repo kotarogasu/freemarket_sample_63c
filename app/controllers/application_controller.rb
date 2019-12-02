adbcd051306addcb949e89006b83c3a974653bda
kotarogasu <kouta2607@gmail.com>
2019-12-02 16:42:21 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,9 +36,9 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def set_current_user
-    @current_user = User.find(session[:user_id])
-  end
+  # def set_current_user
+  #   @current_user = User.find(session[:user_id])
+  # end
   
 
 end
