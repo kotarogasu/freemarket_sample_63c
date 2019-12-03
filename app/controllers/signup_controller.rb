299895516b6ac9fc24101cffbd9bbde13cc27f26
makino3303 <makino3303@icloud.com>
2019-12-03 21:44:50 +0900

    アクション名変更[プルリク修正]

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,7 +1,7 @@
 class SignupController < ApplicationController
-  before_action :validates_step2, only: :step3 # step1のバリデーション
-  before_action :validates_step3, only: :step4 # step2のバリデーション
-  before_action :validates_step4, only: :done # step2のバリデーション
+  before_action :validates_user_registration, only: :sms_confirmation # step2のバリデーション
+  before_action :validates_sms_confirmation, only: address: # step3のバリデーション
+  before_action :validates_address, only: :complete # step4のバリデーション
 
   def social_choice
     @user = User.new
@@ -26,9 +26,8 @@ class SignupController < ApplicationController
     @address = Address.new
   end
 
- def validates_step2
-   # step1で入力された値をsessionに保存
-   params[:user][:birthday] = birthday_join(params[:birthday])
+ def validates_user_registration
+¥   params[:user][:birthday] = birthday_join(params[:birthday])
    session[:nickname] = user_params[:nickname]
    session[:email] = user_params[:email]
    session[:password] = user_params[:password]
@@ -37,12 +36,11 @@ class SignupController < ApplicationController
    session[:last_name_kana] = user_params[:last_name_kana]
    session[:first_name_kana] = user_params[:first_name_kana]
    session[:birthday] = user_params[:birthday]
-       # バリデーション用に、仮でインスタンスを作成する
    @user = User.new(
-     nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+     nickname: session[:nickname],
      email: session[:email],
      password: session[:password],
-     last_name: session[:last_name], # 入力前の情報は、バリデーションに通る値を仮で入れる
+     last_name: session[:last_name],
      first_name: session[:first_name], 
      last_name_kana: session[:last_name_kana], 
      first_name_kana: session[:first_name_kana], 
@@ -52,13 +50,13 @@ class SignupController < ApplicationController
    render '/signup/step2' unless @user.valid?
   end
 
-  def validates_step3
+  def validates_sms_confirmation
     session[:phone_number] = user_params[:phone_number]
     @user = User.new(
-      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+      nickname: session[:nickname], 
       email: session[:email],
       password: session[:password],
-      last_name: session[:last_name], # 入力前の情報は、バリデーションに通る値を仮で入れる
+      last_name: session[:last_name],
       first_name: session[:first_name], 
       last_name_kana: session[:last_name_kana], 
       first_name_kana: session[:first_name_kana], 
@@ -68,7 +66,7 @@ class SignupController < ApplicationController
      render '/signup/step3' unless @user.valid?
   end
 
-  def validates_step4
+  def validates_address
     session[:last_name] = user_params[:last_name]
     session[:first_name] = user_params[:first_name]
     session[:last_name_kana] = user_params[:last_name_kana]
@@ -80,7 +78,7 @@ class SignupController < ApplicationController
     session[:building] = params[:user][:address][:building]
     @user = User.new(
       nickname: session[:nickname],
-      email: session[:email], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
       password: session[:password],
       last_name: session[:last_name],
       first_name: session[:first_name],
@@ -103,7 +101,7 @@ class SignupController < ApplicationController
   def create
     @user = User.new(
       nickname: session[:nickname],
-      email: session[:email], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
       password: session[:password],
       last_name: session[:last_name],
       first_name: session[:first_name],
