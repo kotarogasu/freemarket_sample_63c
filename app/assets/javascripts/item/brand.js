011274079605fba1c8e3d0eb497245611beb38bb
yoshikawa keishi <a130809k@gmail.com>
2019-12-08 13:00:36 +0900

    jsファイルの整理

---
+++ app/assets/javascripts/item/brand.js
@@ -0,0 +1,36 @@
+$(function(){
+  function searchBrand(brand){
+    let brands_list = `<li class="search_result" data-id="${brand.id}" data-name="${brand.name}">${brand.name}</li>`
+    $("#brands-search-list").append(brands_list)
+  }  
+
+  $("#brand-wrap").on('keyup', "#brand-select", function(){
+    let input = $(this).val();
+    $.ajax({
+      type: "GET",
+      url: "/items/brand_find",
+      data: { keyword: input},
+      dataType: "json"
+    })
+    .done(function(brands){
+      $("#brands-search-list").empty();
+      if (brands.length !== 0) {
+        brands.forEach(function(brand){
+          searchBrand(brand);
+        });
+      }else{
+        return false;
+      }
+    })
+    .fail(function(){
+      return false;
+    })
+  });
+
+  $("#brand-wrap").on('click', '.search_result', function(){
+    let brand_name = $(this).data("name");
+    $('#brand-select').val(brand_name);
+    $("#brands-search-list").empty();
+  });
+});
+
