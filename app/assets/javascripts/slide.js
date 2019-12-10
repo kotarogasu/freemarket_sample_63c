d4ce36fa02c2878b333e31d02a2844a207b34696
oicchi <oicchi_ds0703@yahoo.co.jp>
2019-12-10 20:49:47 +0900

    スライドの作成

---
+++ app/assets/javascripts/slide.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.slider').bxSlider({
+      auto: true,
+      pause: 5000,
+  });
+
+  // $('.bx-controls-direction').prepend('<i class="fas fa-chevron-left"></i>');
+  // $('.bx-controls-direction').prepend('<i class="fas fa-chevron-right"></i>');
+
+});
