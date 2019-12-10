304f509046ef69eff002ba80e020632b74a63131
oicchi <oicchi_ds0703@yahoo.co.jp>
2019-12-10 21:05:40 +0900

    削除訂正

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -4,8 +4,4 @@ $(document).ready(function(){
       auto: true,
       pause: 5000,
   });
-
-  // $('.bx-controls-direction').prepend('<i class="fas fa-chevron-left"></i>');
-  // $('.bx-controls-direction').prepend('<i class="fas fa-chevron-right"></i>');
-
 });
