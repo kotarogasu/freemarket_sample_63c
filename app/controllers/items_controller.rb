a28a658c099815893103e12198143f50a5618f46
yoshikawa keishi <a130809k@gmail.com>
2019-12-11 16:59:51 +0900

    コメントアウト等の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -82,20 +82,6 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
-  # def create # 商品編集時の、画像変更を以下に記述しています。
-
-  #   # 変更する画像は、商品にひもづく画像をid順に並べた時の
-  #   # index 番目の画像です。
-  #   target = @item.images.all.order(id: "ASC")[params[:index].to_i]
-  #   target_id = target.id
-
-  #   # 対象画像を削除し、新たな画像を追加する。
-  #   # idを削除した画像のものにしておくことで、元あった場所に画像を入れられるようにする。
-  #   target.purge
-  #   @item.images.attach(params[:image])
-  #   @item.images.last.update(id:target_id)
-  # end
-
   private
 
 
