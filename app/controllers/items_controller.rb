381e3a87146a64430ec6cf93fdb70b7115e875dd
yoshikawa keishi <a130809k@gmail.com>
2019-12-13 20:16:24 +0900

    商品一覧を新着順に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,22 +6,22 @@ class ItemsController < ApplicationController
 
   def index 
     if user_signed_in?
-      @items = Item.where.not(user_id:current_user.id,status:4)
-      @chanel_items = Brand.search("シャネル").items.where.not(user_id:current_user.id,status:4).limit(10)
-      @louisvuitton_items = Brand.search("ルイヴィトン").items.where.not(user_id:current_user.id,status:4).limit(10)
-      @supreme_items = Brand.search("スプリーム").items.where.not(user_id:current_user.id,status:4).limit(10)
-      @nike_items = Brand.search("ナイキ").items.where.not(user_id:current_user.id,status:4).limit(10)
+      @items = Item.where.not(user_id:current_user.id,status:4).order(id: "DESC")
+      @chanel_items = Brand.search("シャネル").items.where.not(user_id:current_user.id,status:4).limit(10).order(id: "DESC")
+      @louisvuitton_items = Brand.search("ルイヴィトン").items.where.not(user_id:current_user.id,status:4).limit(10).order(id: "DESC")
+      @supreme_items = Brand.search("スプリーム").items.where.not(user_id:current_user.id,status:4).limit(10).order(id: "DESC")
+      @nike_items = Brand.search("ナイキ").items.where.not(user_id:current_user.id,status:4).limit(10).order(id: "DESC")
     else
       @items = Item.where.not(status:4)
-      @chanel_items = Brand.search("シャネル").items.where.not(status:4).limit(10)
-      @louisvuitton_items = Brand.search("ルイヴィトン").items.where.not(status:4).limit(10)
-      @supreme_items = Brand.search("スプリーム").items.where.not(status:4).limit(10)
-      @nike_items = Brand.search("ナイキ").items.where.not(status:4).limit(10)
+      @chanel_items = Brand.search("シャネル").items.where.not(status:4).limit(10).order(id: "DESC")
+      @louisvuitton_items = Brand.search("ルイヴィトン").items.where.not(status:4).limit(10).order(id: "DESC")
+      @supreme_items = Brand.search("スプリーム").items.where.not(status:4).limit(10).order(id: "DESC")
+      @nike_items = Brand.search("ナイキ").items.where.not(status:4).limit(10).order(id: "DESC")
     end
-    @ladies_items = @items.get_ladies.limit(10)
-    @mens_items = @items.get_mens.limit(10)
-    @electronics_items = @items.get_electronics.limit(10)
-    @hobbies_items = @items.get_hobbies.limit(10)
+    @ladies_items = @items.get_ladies.limit(10).order(id: "DESC")
+    @mens_items = @items.get_mens.limit(10).order(id: "DESC")
+    @electronics_items = @items.get_electronics.limit(10).order(id: "DESC")
+    @hobbies_items = @items.get_hobbies.limit(10).order(id: "DESC")
   end
 
   def category_find
