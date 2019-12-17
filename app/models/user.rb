ea1cef8497915c2da28a8abf1ca02bec57794918
makino3303 <makino3303@icloud.com>
2019-12-17 09:37:30 +0900

    ログインバリデーションの修正
    登録ユーザーのメールアドレスを正しく入力したときにエラーが出てしまうのを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
+         :recoverable, :rememberable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     # step2入力項目
     validates :nickname,                presence: {message: "ニックネームを入力してください"}, length: {maximum: 20}, unless: :uid?
-    validates :email,                   presence: {message: "メールアドレスを入力してください"}, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, on: :validates_step2, unless: :uid?
+    validates :email,                   presence: {message: "メールアドレスを入力してください"}, uniqueness: {on: :create}, format: { with: VALID_EMAIL_REGEX }
     validates :password,                presence: {message: "パスワードを入力してください"},length: {minimum: 6, maximum: 10}
     validates :last_name,               presence: {message: "姓を入力してください"}, unless: :uid?
     validates :first_name,              presence: {message: "名を入力してください"}, unless: :uid?
@@ -19,7 +19,7 @@ class User < ApplicationRecord
     # validates_acceptance_of :agreement, numericality: {greater_than: 0, message: "選択してください" }
     # validates :prefecture_id, numericality: {greater_than: 0, message: "選択してください" }
 
-        # step3入力項目
+    # step3入力項目
     validates :phone_number,            presence: {message: "会員登録できません"}, length: {minimum: 10, maximum: 11}
 
     def self.from_omniauth(auth)
